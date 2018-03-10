985df4d331b0be6c8e8734471a940c5e7aaec487
killah4605@gmail.com <killah4605@gmail.com>
2018-03-10 03:41:59 +0000

    added destroy ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article Deleted!"
+    redirect_to articles_path
+  end
+  
   def edit
     @article = Article.find(params[:id])
   end
