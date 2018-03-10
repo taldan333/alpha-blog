8819df6ca85f2b94b4c2827785bbd85ff68ed4d7
killah4605@gmail.com <killah4605@gmail.com>
2018-03-10 04:39:41 +0000

    removed redundant code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
   
   def index
     @articles = Article.all
@@ -20,22 +21,18 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article Deleted!"
     redirect_to articles_path
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article Updated!"
       redirect_to article_path(@article)
@@ -49,6 +46,10 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+      @article = Article.find(params[:id])
     end
     
     
